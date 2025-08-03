ad170d804a18b12b763df28d8588a17c71e65392
no4any <martin@karry.de>
2025-08-03 20:51:58 +0200

    Added debug output

--- projects/web/src/lib/mongo/getMongoClient.function.ts
+++ projects/web/src/lib/mongo/getMongoClient.function.ts
@@ -8,6 +8,7 @@ const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD || 'root';
 var mongo: MongoClient | undefined;
 
 export async function getMongoClient(): Promise<MongoClient> {
+    console.log("Connection to", CONNECTION_STRING);
     if(mongo === undefined) {
         console.log("Creating MongoClient");
         mongo = await new MongoClient(CONNECTION_STRING, {
