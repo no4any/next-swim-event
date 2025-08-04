b1d0debb8f5cb8a37a681eb5ddfa11aa36c5b256
no4any <martin@karry.de>
2025-08-04 17:53:46 +0200

    Fixed default value for select

--- projects/web/src/components/input/InputSelect.component.tsx
+++ projects/web/src/components/input/InputSelect.component.tsx
@@ -4,8 +4,8 @@ export default function InputSelect(props: InputSelectProps) {
     const { title, ...rest } = props;
     return <div className="w-full">
         <div className="w-full font-bold">{title}</div>
-        <select className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" {...rest}>
-            {props.options.map((o, i) => <option key={o.value} value={o.value} selected={i == 0}>{o.name}</option>)}
+        <select defaultValue={props.options[0]?.value || ""} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" {...rest}>
+            {props.options.map((o, i) => <option key={o.value} value={o.value}>{o.name}</option>)}
         </select>
     </div>
 }
