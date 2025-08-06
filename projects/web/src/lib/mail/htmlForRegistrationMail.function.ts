d1a93098e676e082aa7dcb643c54ebc095b6a177
no4any <martin@karry.de>
2025-08-06 14:48:48 +0200

    Typo

--- projects/web/src/lib/mail/htmlForRegistrationMail.function.ts
+++ projects/web/src/lib/mail/htmlForRegistrationMail.function.ts
@@ -1,6 +1,6 @@
 export default function htmlForRegistrationMail(link: string, name: string): string {
     return `<h1>Deine Anmeldung zum 11. Gießener 24-Stunden-Schwimmen (2025)</h1>
-    <p>Mit dieser Mail erhältst du den Link zu deinen Anmeldedaten. <b>Behandeln diesen Link vertraulich!</b> Sollten Änderungen nötig sein, kannst du diese beim Check-In am Veranstaltungstag vornehmen lassen.</p>
+    <p>Mit dieser Mail erhältst du den Link zu deinen Anmeldedaten. <b>Behandel diesen Link vertraulich!</b> Sollten Änderungen nötig sein, kannst du diese beim Check-In am Veranstaltungstag vornehmen lassen.</p>
     <p>Teamleiter können über ihren Link auch die Strecken ihrer Mannschaft einsehen.</p>
     <p>Teilnehmer: ${name}</p>
     <p><a href="${link}">Link zu deinen Anmeldedaten</a></p>`
