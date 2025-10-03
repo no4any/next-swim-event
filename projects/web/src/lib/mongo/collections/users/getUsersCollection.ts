1430f04d2a7e3989abc9ad64eeff1b046ec70527
no4any <martin@karry.de>
2025-10-03 16:00:49 +0200

    Fixxed user problem

--- projects/web/src/lib/mongo/collections/users/getUsersCollection.ts
+++ projects/web/src/lib/mongo/collections/users/getUsersCollection.ts
@@ -4,7 +4,7 @@ import { getCollection } from "../../getCollection.function";
 import { SwimCollections } from "../SwimCollection.enum";
 
 export async function getUsersCollection() {
-    const collection = await getCollection<UserWithPassword & WithMongoId>(SwimCollections.USERS);
+    const collection = await getCollection<UserWithPassword>(SwimCollections.USERS);
     await collection.createIndex({ email: 1 }, { unique: true });
     return collection;
 }
