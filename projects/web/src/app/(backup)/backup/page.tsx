189261351d83545aa9b45eea43b35323002766a0
no4any <martin@karry.de>
2025-10-03 16:19:11 +0200

    Added cap color backup

--- projects/web/src/app/(backup)/backup/page.tsx
+++ projects/web/src/app/(backup)/backup/page.tsx
@@ -1,3 +1,4 @@
+import { getCapColors } from "@/lib/mongo/collections";
 import { getSwimmers } from "@/lib/mongo/collections/swimmers/getSwimmers.function";
 import getTeams from "@/lib/mongo/collections/teams/getTeams.function";
 import fs from "fs";
@@ -7,6 +8,7 @@ export const dynamic = 'force-dynamic';
 
 const TEAMS_FILE = "/data/teams.json";
 const SWIMMERS_FILE = "/data/swimmers.json";
+const CAP_COLORS = "/data/cap_colors.json";
 
 export default async function BackupPage() {
     const swimmers = await getSwimmers();
@@ -16,6 +18,9 @@ export default async function BackupPage() {
     const teams = await getTeams();
     const teamsJson = JSON.stringify(teams);
     fs.writeFileSync(TEAMS_FILE, teamsJson);
+
+    const capColorJSON = JSON.stringify(await getCapColors());
+    fs.writeFileSync(CAP_COLORS, capColorJSON);
 
     return <div>Toll!</div>
 }
